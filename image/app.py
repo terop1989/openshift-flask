e610699906b6d04b011bf75d8ba9915a08349a56
Yuriy Kozhin <YKozhin@IBS.RU>
2022-12-06 12:49:46 +0300

    int conversion

--- image/app.py
+++ image/app.py
@@ -7,7 +7,7 @@ status_codes = ('200','404')
 
 @app.route('/')
 def route_root():
-    return ('Status...', random.choice(status_codes))
+    return ('Status...', int(random.choice(status_codes)))
 
 
 if __name__ == '__main__':
