a90885a1c012dadc1fb86e59f8d1fac9c16caf86
Yuriy Kozhin <YKozhin@IBS.RU>
2022-12-08 16:51:34 +0300

    add logging into application

--- image/app.py
+++ image/app.py
@@ -1,14 +1,27 @@
-import random
-
+import logging, random
 from flask import Flask
+
+def get_stream_logger():
+  log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+  sh = logging.StreamHandler()
+  sh.setFormatter(logging.Formatter(log_format))
+  sh.setLevel(logging.INFO)
+
+  logger = logging.getLogger("example-app")
+  logger.setLevel(logging.INFO)
+  logger.addHandler(sh)
+  return logger
 
 app = Flask(__name__)
 status_codes = [200, 404]
+logger = get_stream_logger()
 
 @app.route('/')
 def route_root():
-    return ('Status...', random.choice(status_codes))
-
+  status = random.choice(status_codes)
+  logger.info("calling root route")
+  return ('Status ' + str(status) + '\n', status)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, threaded=True)
+  app.run('0.0.0.0', 5000, threaded=True)
